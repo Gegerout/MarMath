e69f71d91a0aac928dc460daca19086caac857d8
Gegerout <selimchik06@gmail.com>
2023-09-09 20:03:49 +0300

    update

---
+++ lib/states.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:math_app/api.dart';
+import 'package:image/image.dart' as img;
+
+final sendListsProvider = ChangeNotifierProvider((ref) => SendListsNotifier());
+
+class SendListsNotifier extends ChangeNotifier {
+  List<dynamic> result = [];
+  List<dynamic> actions = [];
+  List<dynamic> normalResult = [];
+  img.Image? imageCode;
+
+  Future<void> getAnswers(
+      List<dynamic> list1, List<dynamic> list2) async {
+    final data = await Api().getAnswers(list1, list2);
+    result = data[0];
+    actions = data[1];
+    notifyListeners();
+  }
+
+  Future<void> findNormal(
+      List<dynamic> list1, List<dynamic> list2) async {
+    final data = await Api().findNormal(list1, list2);
+    normalResult = data[0];
+    final decodedImage = img.decodeImage(
+      latin1.encode(data[1]),
+    );
+    imageCode = decodedImage;
+    notifyListeners();
+  }
+
+  void clearGaussStates() {
+    result = [];
+    actions = [];
+    notifyListeners();
+  }
+
+  void clearNormalStates() {
+    normalResult = [];
+    imageCode = null;
+    notifyListeners();
+  }
+}
