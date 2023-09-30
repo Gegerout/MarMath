a7348fecd543d13e9ecc79a5c2dd305e4331d917
Gegerout <selimchik06@gmail.com>
2023-09-30 22:07:11 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:math_app/ads_screen.dart';
+import 'package:math_app/compile_code_screen.dart';
 import 'package:math_app/find_normal_screen.dart';
 import 'package:math_app/gauss_method_screen.dart';
 import 'package:math_app/gauss_screen.dart';
@@ -41,7 +42,7 @@ class MyApp extends ConsumerWidget {
       ),
       home: const Scaffold(
         resizeToAvoidBottomInset: false,
-        body: HomeScreen(),
+        body: CompileCodeScreen(),
     // Column(
         //   children: [
         //     Row(
