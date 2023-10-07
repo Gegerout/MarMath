04ee5a489a133e7b156d8a4054a0421c6894dcf5
Gegerout <selimchik06@gmail.com>
2023-10-07 21:46:19 +0300

    update

--- lib/api.dart
+++ lib/api.dart
@@ -3,24 +3,24 @@ import 'dart:typed_data';
 import 'package:dio/dio.dart';
 
 class Api {
-  Future<List<dynamic>> getAnswers(List<dynamic> values, List<dynamic> equals) async {
+  Future<List<dynamic>> getAnswers(
+      List<dynamic> values, List<dynamic> equals) async {
     final Dio dio = Dio();
 
-    final res = await dio.post("http://evgeniymuravyov.pythonanywhere.com/process_lists", data: {
-      "list1": values,
-      "list2": equals
-    });
+    final res = await dio.post(
+        "http://evgeniymuravyov.pythonanywhere.com/process_lists",
+        data: {"list1": values, "list2": equals});
 
     return [res.data["result"], res.data["actions"]];
   }
 
-  Future<List<dynamic>> findNormal(List<dynamic> list1, List<dynamic> list2) async {
+  Future<List<dynamic>> findNormal(
+      List<dynamic> list1, List<dynamic> list2) async {
     final Dio dio = Dio();
 
-    final res = await dio.post("http://evgeniymuravyov.pythonanywhere.com/find_normal", data: {
-      "list1": list1,
-      "list2": list2
-    });
+    final res = await dio.post(
+        "http://evgeniymuravyov.pythonanywhere.com/find_normal",
+        data: {"list1": list1, "list2": list2});
 
     return [res.data["result"], res.data["image"]];
   }
@@ -28,7 +28,8 @@ class Api {
   Future<String> compileCode(String code) async {
     final Dio dio = Dio();
 
-    final res = await dio.post("http://evgeniymuravyov.pythonanywhere.com/compile_code", data: {
+    final res = await dio
+        .post("http://evgeniymuravyov.pythonanywhere.com/compile_code", data: {
       "code": code,
     });
 
@@ -38,11 +39,12 @@ class Api {
   Future<String> sendWolfram(String input) async {
     final Dio dio = Dio();
 
-    final res = await dio.post("http://api.wolframalpha.com/v1/simple", queryParameters: {
-      "appid": "K6G3JW-7UPEA57PVT",
-      "layout": "divider",
-      "i": input
-    });
+    final res = await dio.post("http://api.wolframalpha.com/v1/simple",
+        queryParameters: {
+          "appid": "K6G3JW-7UPEA57PVT",
+          "layout": "divider",
+          "i": input
+        });
     return res.data;
   }
 }
